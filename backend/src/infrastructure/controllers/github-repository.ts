4edfe2969ec6cdba5075c810b2094bff0d916eef
Oleh Pykulytsky <pykulytsky@gmail.com>
2025-06-28 01:01:28 +0300

    feat(be): sketch repositories controller

--- backend/src/infrastructure/controllers/github-repository.ts
+++ backend/src/infrastructure/controllers/github-repository.ts
@@ -1,4 +1,59 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Version,
+} from '@nestjs/common';
+import { AddRepositoryDto, RepositoryDto } from '../dtos/repository.dto';
+import { FetchRepositoriesUseCase } from 'src/application/use-cases/repository/fetch-repositories.use-case';
+import { UpdateRepositoryUseCase } from 'src/application/use-cases/repository/update-repository.use-case';
+import { DeleteRepositoryUseCase } from 'src/application/use-cases/repository/delete-repository.use-case';
+import { AddRepositoryUseCase } from 'src/application/use-cases/repository/add-repository.user-case';
 
 @Controller('github-repositories')
-export class GithubRepositoryController {}
+export class GithubRepositoryController {
+  constructor(
+    private readonly fetchRepositoriesUseCase: FetchRepositoriesUseCase,
+    private readonly addRepositoryUseCase: AddRepositoryUseCase,
+    private readonly updateRepositoryUseCase: UpdateRepositoryUseCase,
+    private readonly deleteRepositoryUseCase: DeleteRepositoryUseCase,
+  ) {}
+
+  @Version('1')
+  @Get('')
+  async fetchUserRepositories(): Promise<Array<RepositoryDto>> {
+    const userId = ''; // TODO: get it from request once auth is done
+
+    return await this.fetchRepositoriesUseCase.executeForUser(userId);
+  }
+
+  @Version('1')
+  @Post('')
+  async addRepository(@Body() input: AddRepositoryDto): Promise<RepositoryDto> {
+    const userId = ''; // TODO: get it from request once auth is done
+
+    return await this.addRepositoryUseCase.execute(
+      userId,
+      input.owner,
+      input.name,
+    );
+  }
+
+  @Version('1')
+  @Patch(':id')
+  async updateRepository(@Param('id') id: string): Promise<RepositoryDto> {
+    const userId = ''; // TODO: get it from request once auth is done
+    return await this.updateRepositoryUseCase.execute(id, userId);
+  }
+
+  @Version('1')
+  @Delete(':id')
+  async deleteRepository(@Param('id') id: string): Promise<RepositoryDto> {
+    const userId = ''; // TODO: get it from request once auth is done
+    return await this.deleteRepositoryUseCase.execute(id, userId);
+  }
+}
--- backend/src/infrastructure/modules/github-repository.ts
+++ backend/src/infrastructure/modules/github-repository.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
 import { GithubRepositoryController } from '../controllers/github-repository';
+import { FetchRepositoriesUseCase } from 'src/application/use-cases/repository/fetch-repositories.use-case';
+import { AddRepositoryUseCase } from 'src/application/use-cases/repository/add-repository.user-case';
+import { UpdateRepositoryUseCase } from 'src/application/use-cases/repository/update-repository.use-case';
+import { DeleteRepositoryUseCase } from 'src/application/use-cases/repository/delete-repository.use-case';
+import { PrismaService } from '../services/prisma.service';
+import { PrismaRepositoryRepository } from '../repositories/repository.repository';
+import { GithubService } from '../services/github.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [HttpModule],
   controllers: [GithubRepositoryController],
+  providers: [
+    PrismaService,
+    ConfigService,
+
+    { provide: 'IRepositoryRepository', useClass: PrismaRepositoryRepository },
+    { provide: 'IRepositoryHostingService', useClass: GithubService },
+
+    FetchRepositoriesUseCase,
+    AddRepositoryUseCase,
+    UpdateRepositoryUseCase,
+    DeleteRepositoryUseCase,
+  ],
 })
 export class GithubRepositoryModule {}
